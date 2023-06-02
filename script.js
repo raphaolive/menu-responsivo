72231955d683de357fd8ca643de5ca9787210852
Raphael O. Santos <raphael.oliveira@inovan.do>
2023-06-02 10:07:50 -0300

    feat: menu icon, bottom gradient overlay

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-$('.mobile-menu-icon').on('click', () => {
-    $('.mobile-menu').toggleClass('open')
-})
+$(".mobile-menu-icon").on("click", () => {
+  $(".mobile-menu").toggleClass("open");
+  let src = $(".mobile-menu").hasClass("open")
+    ? "./assets/img/close_white_36dp.svg"
+    : "./assets/img/menu_white_36dp.svg";
+
+  $(".icon").attr("src", src);
+});
